a8b2ea20f51c97943f14d0258ecf3728a112a7e4
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-08-28 13:00:19 +0530

    [feedback] Refactor: changes during feedback

--- scripts.js
+++ scripts.js
@@ -1,30 +1,35 @@
 function validation(){
-    var username = document.getElementById('username').value;
-    var email = document.getElementById('useremail').value;
-    var pswd = document.getElementById('userpassword').value;
+    var username = document.getElementsByName('username')[0].value;
+    var email = document.getElementsByName('useremail')[0].value;
+    var pswd = document.getElementsByName('userpassword')[0].value;
 
     var usernamecheck = /^[A-za-z.]{3,30}$/;
     var userpswdcheck = /(?=.*[A-Z])(?=.*[0-9])(?=.*[a-z])(?=.*[+@#$%^&*!.()-]).{8,}/;
     var emailcheck = /^[a-zA-Z0-9]{1,}[._+-]?[a-zA-Z0-9]{1,}[@][a-zA-Z0-9]{1,}([.][a-zA-Z]{2,4}){1,3}$/;
     
+    var counter=0;
     if(usernamecheck.test(username)){
         document.getElementById('nameerror').innerHTML = "";
     }else{
+        counter++;
         document.getElementById('nameerror').innerHTML = "Username is invalid";
-        return false;
     }
 
     if(emailcheck.test(email)){
         document.getElementById('emailerror').innerHTML = "";
     }else{
+        counter++;
         document.getElementById('emailerror').innerHTML = "Email is invalid";
-        return false;
     }
 
     if(userpswdcheck.test(pswd)){
         document.getElementById('passworderror').innerHTML = "";
     }else{
+        counter++;
         document.getElementById('passworderror').innerHTML = "Password is invalid";
+    }
+    if(counter > 0)
+    {
         return false;
     }
     alert("Form Successfully Submitted");
